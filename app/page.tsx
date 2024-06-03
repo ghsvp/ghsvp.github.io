1b214996d5d41c4477866c8424a81f0d41bb4a6a
Yubo Cao <cao2006721@gmail.com>
2024-06-03 07:38:27 -0400

    feat: :sparkles: Update the home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,19 @@ import Footer from "@/components/Footer/Footer";
 export default function Home() {
   return (
     <>
-      <main className="px-6 md:px-18 max-w-xl md:max-w-5xl xl:max-w-[calc(100%-24rem)] mx-auto space-y-12 md:space-y-20">
+      <main className="px-6 md:px-18 max-w-xl md:max-w-5xl xl:max-w-[calc(100%-24rem)] mx-auto space-y-4 md:space-y-8">
         <Hero />
-        {/* <section className="space-y-4">
-          <h1 className="text-primary-1-500">
-            About Us
-            <Marker title="About Us" icon="info" id="about" />
-          </h1>
-        </section> */}
+        <section className="leading-relaxed p-8 bg-primary-2-50 ">
+          <p className="text-sm lg:text-lg">
+            <span className="text-2xl lg:text-3xl font-bold text-primary-2-500">
+              {"The Georgia High School Voting Project "}
+            </span>
+            is a student-led organization that aims to help Georgia high school
+            students register to vote. We believe that every voice matters and
+            that every vote counts. Our goal is to help young people make their
+            voices heard in the political process.
+          </p>
+        </section>
         <Footer />
       </main>
     </>
