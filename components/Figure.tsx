27a610bea307b2d7185f276d6a5bcbd339e149f1
Yubo Cao <cao2006721@gmail.com>
2024-06-03 08:12:22 -0400

    feat: :sparkles: Past action page

---
+++ components/Figure.tsx
@@ -0,0 +1,31 @@
+import Image, { StaticImageData } from "next/image";
+
+export interface FigureProps {
+  src: string | StaticImageData;
+  alt?: string;
+  caption: string;
+  className?: string;
+  imageClassName?: string;
+  style?: React.CSSProperties;
+}
+
+export default function Figure({
+  src,
+  alt,
+  caption,
+  className,
+  imageClassName,
+  style,
+}: FigureProps) {
+  return (
+    <figure className={className} style={style}>
+      <Image
+        src={src}
+        alt={alt || caption}
+        objectFit="cover"
+        className={imageClassName || "aspect-[2/1] object-cover"}
+      />
+      <figcaption className="lg:text-lg text-slate-500 mt-2">{caption}</figcaption>
+    </figure>
+  );
+}
