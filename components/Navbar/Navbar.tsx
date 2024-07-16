9e4923e480362f26ed52e7e021f958eb5a88f8a9
Yubo Cao <cao2006721@gmail.com>
2024-07-15 22:49:37 -0400

    feat: update with nested navbar

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -12,40 +12,41 @@ type NavbarItemProps = {
 
 type NavbarFolderProps = {
   text: string;
+  icon: string;
   items: NavbarItemProps[];
 };
 
-function NavbarFolderDesktop({ text, items }: NavbarFolderProps) {
-  return (
-    <div className="group">
-      <div className="text-sm font-bold leading-7 transition-all group-hover:font-bold">
-        {text}
-      </div>
-      <div className="invisible flex flex-col gap-2 group-hover:visible">
-        {items.map((item) => (
-          <Link href={item.href} key={item.href}>
-            <div className="text-sm font-normal leading-7 transition-all group-hover:font-bold">
-              {item.text}
-            </div>
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
-}
+type NavbarFolderMobileProps = {
+  text: string;
+  icon: string;
+  items: NavbarItemProps[];
+  setExpanded: (state: boolean) => void;
+};
 
-function NavbarFolderMobile({ text, items }: NavbarFolderProps) {
-  const [expanded, setExpanded] = useState(false);
-}
+type NavbarItemMobileProps = {
+  item: NavbarItemProps;
+  activeState?: boolean;
+  setExpanded: (state: boolean) => void;
+};
 
-const NavbarItem = (
-  items: NavbarItemProps,
-  setExpanded: (state: boolean) => void,
-) => {
-  const { icon, text, href } = items;
+const NavbarItemMobile = ({
+  item,
+  activeState,
+  setExpanded,
+}: NavbarItemMobileProps) => {
+  const { icon, text, href } = item;
+
   const [active, setActive] = useState(false);
 
+  if (activeState) {
+    setActive(activeState);
+  }
+
   useEffect(() => {
+    if (activeState) {
+      setActive(activeState);
+      return;
+    }
     if (typeof window !== "undefined") {
       setActive(window.location.pathname === href);
     }
@@ -53,7 +54,7 @@ const NavbarItem = (
       setActive(window.location.pathname === href);
     }, 1000);
     return () => clearInterval(interval);
-  }, [href, setActive]);
+  }, [href, setActive, activeState]);
 
   const iconComponent = (
     <div
@@ -82,12 +83,87 @@ const NavbarItem = (
   );
 };
 
+function NavbarFolderMobile({
+  text,
+  icon,
+  items,
+  setExpanded: setExpandedParent,
+}: NavbarFolderMobileProps) {
+  const [expanded, setExpanded] = useState(false);
+
+  const iconComponent = (
+    <div
+      className={`material-symbols-rounded group-hover:material-symbols-filled flex h-8 w-8 items-center justify-center transition-all`}
+    >
+      {icon}
+    </div>
+  );
+  const textComponent = (
+    <div className={`text-sm font-normal leading-7 transition-all`}>{text}</div>
+  );
+
+  return (
+    <div
+      className={`group flex w-full flex-col ${expanded ? "rounded-xl bg-primary-1-100" : ""}`}
+    >
+      <div
+        className={`group inline-flex w-full items-center justify-start gap-1 px-2 py-1 text-lg transition-all ease-in-out`}
+        onClick={() => setExpanded(!expanded)}
+      >
+        {iconComponent}
+        {textComponent}
+      </div>
+      <div
+        className={`flex flex-col overflow-hidden transition-all ease-in-out ${expanded ? "mx-2 mb-2 max-h-[100vh] py-2" : "max-h-0"} rounded-lg bg-primary-1-50`}
+      >
+        {items.map((item) => (
+          <NavbarItemMobile
+            item={item}
+            key={item.href}
+            activeState={false}
+            setExpanded={setExpandedParent}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function NavbarItemDesktop({ text, href }: NavbarItemProps) {
+  return (
+    <Link
+      className={`text-sm font-bold leading-none text-slate-600 transition-all ease-in-out hover:text-slate-800`}
+      href={href}
+      key={href}
+    >
+      {text}
+    </Link>
+  );
+}
+
+function NavbarFolderDesktop({ text, items }: NavbarFolderProps) {
+  return (
+    <div className="group relative flex h-[0.875rem] flex-grow-0 flex-col items-center overflow-clip hover:overflow-visible transition-all">
+      <div className="text-sm font-bold leading-none text-slate-600 transition-all hover:text-slate-800 group-hover:font-bold">
+        {text}
+      </div>
+      <div className="h-fit max-h-0 max-w-0 transition-all group-hover:max-h-[100vh] group-hover:max-w-[100vw]">
+        <div className="flex flex-col gap-4 rounded-sm bg-white p-4 shadow-md">
+          {items.map((item) => (
+            <NavbarItemDesktop {...item} key={item.href} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function NavbarMobile({
   items,
   expanded,
   setExpanded,
 }: {
-  items: NavbarItemProps[];
+  items: (NavbarItemProps | NavbarFolderProps)[];
   expanded: boolean;
   setExpanded: (expanded: boolean) => void;
 }) {
@@ -107,12 +183,29 @@ function NavbarMobile({
               menu_open
             </div>
           </div>
-
-          {items.map((item) => NavbarItem(item, setExpanded))}
+          {items.map((item) => {
+            if ("items" in item) {
+              return (
+                <NavbarFolderMobile
+                  {...item}
+                  key={item.text}
+                  setExpanded={setExpanded}
+                />
+              );
+            } else {
+              return (
+                <NavbarItemMobile
+                  item={item}
+                  key={item.href}
+                  setExpanded={setExpanded}
+                />
+              );
+            }
+          })}
         </div>
       </div>
       <div
-        className={`bg-slate-900 fixed bottom-0 left-0 right-0 top-0 z-40 h-[100vh] w-[100vw] bg-opacity-50 backdrop-blur-sm transition-opacity ease-in-out ${
+        className={`fixed bottom-0 left-0 right-0 top-0 z-40 h-[100vh] w-[100vw] bg-slate-900 bg-opacity-50 backdrop-blur-sm transition-opacity ease-in-out ${
           expanded ? "opacity-100" : "pointer-events-none opacity-0"
         }`}
         onClick={() => setExpanded(false)}
@@ -121,26 +214,29 @@ function NavbarMobile({
   );
 }
 
-function NavbarDesktop({ items }: { items: NavbarItemProps[] }) {
+function NavbarDesktop({
+  items,
+}: {
+  items: (NavbarItemProps | NavbarFolderProps)[];
+}) {
   return (
-    <div className="invisible h-12 w-96 items-center justify-between sm:visible sm:flex">
-      {items.map((item: NavbarItemProps) => {
-        const { text, href } = item;
-        return (
-          <Link
-            className={`text-sm font-bold leading-none text-slate-600 transition-all ease-in-out hover:text-slate-800`}
-            href={href}
-            key={href}
-          >
-            {text}
-          </Link>
-        );
+    <div className="invisible h-12 w-96 items-center justify-between sm:visible sm:flex transition-all">
+      {items.map((item) => {
+        if ("items" in item) {
+          return <NavbarFolderDesktop {...item} key={item.text} />;
+        } else {
+          return <NavbarItemDesktop {...item} key={item.href} />;
+        }
       })}
     </div>
   );
 }
 
-export default function Navbar({ items }: { items: NavbarItemProps[] }) {
+export default function Navbar({
+  items,
+}: {
+  items: (NavbarItemProps | NavbarFolderProps)[];
+}) {
   const [expanded, setExpanded] = useState(false);
 
   return (
@@ -172,7 +268,7 @@ export default function Navbar({ items }: { items: NavbarItemProps[] }) {
   );
 }
 
-export const NavbarData = [
+export const NavbarData: (NavbarFolderProps | NavbarItemProps)[] = [
   {
     text: "Home",
     href: "/",
@@ -180,8 +276,19 @@ export const NavbarData = [
   },
   {
     text: "Action",
-    href: "/action",
     icon: "history",
+    items: [
+      {
+        text: "Outreach",
+        href: "/action/outreach",
+        icon: "communities",
+      },
+      {
+        text: "Past Drives",
+        href: "/action/past-drives",
+        icon: "history",
+      },
+    ],
   },
   {
     text: "Get Involved",
@@ -198,7 +305,6 @@ export const NavbarData = [
     href: "/contact",
     icon: "email",
   },
-  // TODO: make a nested navbar
 ];
 
 export function ConstNavbar() {
